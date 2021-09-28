5b5896aa0d20ac15f1e5e08a8ed093d236a3b362
garateHegoi <71440711+garateHegoi@users.noreply.github.com>
2021-09-28 10:40:53 +0200

    Update WriteFile.java
    .

--- Java/01-Erronka/src/main/java/eus/uni/dam/WriteFile.java
+++ Java/01-Erronka/src/main/java/eus/uni/dam/WriteFile.java
@@ -4,36 +4,55 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
 public class WriteFile {
-	   public static void main(String[] args) {
-	      BufferedWriter bw = null;
-	      try {
-		 String mycontent = Pelicula peli:peliDao.getPeliculas();
-		 
-		 File file = new File("C:/datuak.txt");
-		 
-		  if (!file.exists()) {
-		     file.createNewFile();
-		  }
+	public static void main(String[] args) {
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		  FileWriter fw = new FileWriter(file);
-		  bw = new BufferedWriter(fw);
-		  bw.write(mycontent);
-	          System.out.println("File written Successfully");
+		BufferedWriter bw = null;
 
-	      } catch (IOException ioe) {
-		   ioe.printStackTrace();
+		try {
+			PeliculaDao peliDao = appContext.getBean(PeliculaDaoMemory.class);
+
+			System.out.println("Datuak gordetezen:");
+			for (Pelicula peli : peliDao.getPeliculas()) {
+				System.out.println(peli);
+			}
+
+			File file = new File("C:\\Users\\garate.hegoi\\Desktop\\datuak\\datuak.txt");
+			FileWriter fr = null;
+			BufferedWriter br = null;
+			String dataWithNewLine = peliDao.getPeliculas() + System.getProperty("line.separator");
+			try {
+				fr = new FileWriter(file);
+				br = new BufferedWriter(fr);
+				for (int i = peliDao.getPeliculas().size(); i > 0; i--) {
+					br.write(dataWithNewLine);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					br.close();
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+			} catch (Exception ex) {
+				System.out.println("Error in closing the BufferedWriter" + ex);
+			}
 		}
-		finally
-		{ 
-		   try{
-		      if(bw!=null)
-			 bw.close();
-		   }catch(Exception ex){
-		       System.out.println("Error in closing the BufferedWriter"+ex);
-		    }
-		}
-	   }
-	   ((AnnotationConfigApplicationContext) appContext).close();
+		((AnnotationConfigApplicationContext) appContext).close();
 	}
+}
