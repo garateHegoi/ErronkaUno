55c80d1cfe3693e094a10fa0152d50425bf909f4
MarkelUribe <75306077+MarkelUribe@users.noreply.github.com>
2021-09-30 10:44:18 +0200

    android taula

--- Android/app/src/main/java/com/example/erronkauno/MainActivity.java
+++ Android/app/src/main/java/com/example/erronkauno/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.erronkauno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TableLayout tl;
+    int n = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tl = (TableLayout) findViewById(R.id.taula);
+
+    }
+
+    public void gehitu(View view){
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+
+        for (int i = 0; i<4;i++){
+            Button b = new Button(this);
+            b.setClickable(false);
+            b.setText(n+"_x"+i);
+            tr.addView(b);
+        }
+        tl.addView(tr);
+        Log.d("klik", "sdfil");
     }
 }
