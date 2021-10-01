31f2ea520329776753125c041752bbe25f9a15ca
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-10-01 14:21:48 +0200

    Erik Le sabee

--- Android/app/src/main/java/com/example/erronkauno/MainActivity.java
+++ Android/app/src/main/java/com/example/erronkauno/MainActivity.java
@@ -1,45 +1,54 @@
 package com.example.erronkauno;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    TableLayout tl;
+    ListView lv;
     int n = 0;
-
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tl = (TableLayout) findViewById(R.id.taula);
-        ArrayList<Products> produktuak = aldatzailea();
-        for (Products produktua : produktuak) {
-            TableRow tr = new TableRow(this);
-            Button id = new Button(this);
-            id.setText(String.valueOf(produktua.getID()));
-            tr.addView(id);
-            Button name = new Button(this);
-            name.setText(produktua.getName());
-            tr.addView(name);
-            Button price = new Button(this);
-            price.setText(String.valueOf(produktua.getList_price()));
-            tr.addView(price);
-            tl.addView(tr);
-        }
+        Toolbar toolbar = (Toolbar)  findViewById(R.id.nav_search);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        lv = (ListView) findViewById(R.id.tv_emptyTextView);
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,aldatzaileanames());
+        lv.setAdapter(adapter);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menudos) {
+        MenuInflater mf = getMenuInflater();
+        mf.inflate(R.menu.menu,menudos);
+
+        MenuItem search = (MenuItem) findViewById(R.id.nav_search);
+        SearchView sv = (SearchView) MenuItemCompat.getActionView(search);
+
+        return super.onCreateOptionsMenu(menudos);
     }
 
     public ArrayList<String> irakurri() {
@@ -79,7 +88,23 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Products p = new Products(Integer.parseInt(produktuak_aldatuta[0]), produktuak_aldatuta[1], produktuak_aldatuta[2], produktuak_aldatuta[3], produktuak_aldatuta[4], produktuak_aldatuta[5], Boolean.parseBoolean(produktuak_aldatuta[6]), Boolean.parseBoolean(produktuak_aldatuta[7]), Boolean.parseBoolean(produktuak_aldatuta[8]), Boolean.parseBoolean(produktuak_aldatuta[9]));
                 result.add(p);
+
+
             }
             return result;
         }
+    public  ArrayList<String> aldatzaileanames(){
+        ArrayList<String> produktuak = irakurri();
+        ArrayList<String> names = new ArrayList<String>();
+        for (String produktu : produktuak) {
+            String[] produktuak_aldatuta = produktu.split("[,]", 0);
+            for (String gauza: produktuak_aldatuta){
+                Log.d("xd", gauza);
+            }
+
+            names.add(produktuak_aldatuta[1]);
+
+        }
+        return names;
+    }
     }
