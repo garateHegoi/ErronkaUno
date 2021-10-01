f20a78a2cdc9b65c5403e425706343f7b7d198a2
Erik Gallego <XD9002@gmail.com>
2021-10-01 08:26:18 +0200

    Cambio a Products

--- Java/01-ErronkaUno/src/main/java/eus/uni/dam/WriteFile.java
+++ Java/01-ErronkaUno/src/main/java/eus/uni/dam/WriteFile.java
@@ -16,14 +16,12 @@ public class WriteFile {
 	
 	public static void main(String[] args) {
 		
-		List<Customer> lista= new ArrayList<Customer>();
+		List<Products> lista= new ArrayList<Products>();
 		
 		String DB_URL = "jdbc:postgresql://192.168.65.21/ErronkaUno";
 		String USER = "openpg";
 		String PASS = "openpgpwd";
-		String QUERY = "SELECT id, \"name\", company_id, create_date, display_name, \"date\", title, parent_id, \"ref\", lang, tz, user_id, vat, website, \"comment\", credit_limit, active, employee, \"function\", \"type\", street, street2, zip, city, state_id, country_id, partner_latitude, partner_longitude, email, phone, mobile, is_company, industry_id, color, partner_share, commercial_partner_id, commercial_company_name, company_name, create_uid, write_uid, write_date, message_main_attachment_id, email_normalized, message_bounce, signup_token, signup_type, signup_expiration, team_id, partner_gid, additional_info, phone_sanitized, debit_limit, last_time_entries_checked, invoice_warn, invoice_warn_msg, supplier_rank, customer_rank, sale_warn, sale_warn_msg, picking_warn, picking_warn_msg, calendar_last_notif_ack, purchase_warn, purchase_warn_msg, website_id, is_published\n"
-				+ "FROM public.res_partner;\n"
-				+ "";
+		String QUERY = "SELECT id, 'name', description, list_price, volume, weight, sale_ok, purchase_ok,is_published, active FROM public.product_template";
 		
 		try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		         Statement stmt = conn.createStatement();
@@ -31,21 +29,21 @@ public class WriteFile {
 
 		         while (rs.next()) {
 		            // Retrieve by column name
-		        	Customer customer = new Customer(rs.getInt("id"),rs.getString("name"),rs.getString("email"),rs.getString("calendar_last_notif_ack"));		      
-		        	lista.add(customer);
-		        	System.out.println(customer);
+		        	Products product = new Products(rs.getInt("id"),rs.getString("name"),rs.getString("list_price"),rs.getString("description"),rs.getString("volume"),rs.getString("weight"),rs.getBoolean("sale_ok"),rs.getBoolean("purchase_ok"),rs.getBoolean("active"),rs.getBoolean("is_published"));		      
+		        	lista.add(product);
+		        	System.out.println(product);
 		         }
 		      } catch (SQLException e) {
 		         e.printStackTrace();
 		      }
 		
 		try {
-		      File myObj = new File("db\\customers.txt");		      
+		      File myObj = new File("db\\products.txt");		      
 		      if (myObj.createNewFile()) {
 		        System.out.println("File created: " + myObj.getName());
-		        FileWriter myWriter = new FileWriter("db\\customers.txt");
-		        for(Customer cus:lista) {
-		        	myWriter.write(cus.getID()+","+cus.getName()+","+cus.getEmail()+","+cus.getDate()+"\n");
+		        FileWriter myWriter = new FileWriter("db\\products.txt");
+		        for(Products cus:lista) {
+		        	myWriter.write(cus.getID()+","+cus.getName()+","+cus.getList_price()+","+cus.isActive()+"\n");
 		        }
 		        myWriter.close();
 		      } else {
